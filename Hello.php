34325bf2d2199b6d5fae0a9411bc6a624e90acf7
vansia07 <102646875+vansia07@users.noreply.github.com>
2022-03-30 12:53:18 +0530

    Update Hello.php

--- Hello.php
+++ Hello.php
@@ -1,10 +1,12 @@
+<html>
+  <body>
+    
 <?php
 ECHO "Hello!<br>";
 echo "Welcome to Developer News<br>";
 EcHo "Enjoy all of the ad-free articles<br>";
 ?>
-<html>
-  <body>
+
 <a href="<?php echo 'index.php'; ?>">HOME</a>
 
   </body>
